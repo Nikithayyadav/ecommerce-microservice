1dbb38575916172f0a87271b8b3ed8a568d63e6b
Nikitha yadav <CHANDAVENANIKITHA9@GMAIL.COM>
2026-07-20 19:58:45 +0530

    Update EcommerceApplication.java

--- src/main/java/com/ecommerce/EcommerceApplication.java
+++ src/main/java/com/ecommerce/EcommerceApplication.java
@@ -11,6 +11,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class EcommerceApplication {
     public static void main(String[] args) {
         SpringApplication.run(EcommerceApplication.class, args);
-    }
+    } 
 } 
   
